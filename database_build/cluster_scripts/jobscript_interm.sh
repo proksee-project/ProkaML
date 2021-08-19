4a3eb130680bb9b04e3ceac6a062ba707dc84a1e
asahaman <arnab22.iitkgp@gmail.com>
2021-08-19 09:54:33 -0500

    readme typo

--- database_build/cluster_scripts/jobscript_interm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=biopython
-#SBATCH --output=/home/CSCScience.ca/amandal/proksee-database/snakemake/database_build/job_%J_out.txt
-#SBATCH --error=/home/CSCScience.ca/amandal/proksee-database/snakemake/database_build/job_%J_err.txt
-#SBATCH --partition=NMLResearch
-
-python metadata_print_fileindex.py arnab22.iitkgp@gmail.com 51efc5e252e63fae1155a67c802bdd8e3e09 id_list_interm interm_species_metadata $SLURM_ARRAY_TASK_ID
